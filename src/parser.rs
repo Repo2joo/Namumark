33bac6435fdfddc3b4785b1371d26b1358ce051a
Duswnsey <hazer24879@gmail.com>
2025-05-28 01:47:21 +0900

    역시개발은시작이재밌어

--- src/parser.rs
+++ src/parser.rs
@@ -1,18 +1,100 @@
+use core::panic;
+
 use crate::structs::{Compiler, Expect, Link, LinkType, Objects, RenderObject};
 
 pub fn parse_first(compiler: &mut Compiler, close: Expect) -> RenderObject {
-    let mut namumarkresult:Vec<Objects>;
-    let mut result:RenderObject;
-    RenderObject::NopForLink
+    let mut namumarkresult: Vec<Objects> = Vec::new();
+    let mut result: RenderObject = RenderObject::NopNopNop;
+    let mut close = close;
+    parsing_listener(compiler, &close, &namumarkresult, &mut result);
+    while namumarker(compiler, &mut close, &mut namumarkresult, &mut result) {};
+    result
 }
-fn parsing_listener(compiler: &mut Compiler, close: Expect, namumarkresult: Vec<Objects>, result:&mut RenderObject) {
+fn parsing_listener(
+    compiler: &mut Compiler,
+    close: &Expect,
+    namumarkresult: &Vec<Objects>,
+    result: &mut RenderObject,
+) {
     match close {
         Expect::None => *result = RenderObject::NopNopNop,
-        Expect::Link => *result = RenderObject::Link(Link {
-            to: String::new(),
-            show: Some(Vec::new()),
-            link_type: LinkType::Hyper,
-        }),
+        Expect::Link => {
+            *result = RenderObject::Link(Link {
+                to: String::new(),
+                show: Some(Vec::new()),
+                link_type: LinkType::Hyper,
+            })
+        }
         Expect::Link2 => panic!(),
     }
+}
+fn namumarker(
+    compiler: &mut Compiler,
+    close:&mut Expect,
+    namumarkresult: &mut Vec<Objects>,
+    mut result: &mut RenderObject,
+) -> bool {
+    if let Some(Objects::Char(ch)) = compiler.current() {
+        let ch = ch.to_owned();
+        if matches!(close, Expect::Link) {
+            if let RenderObject::Link(link) = result {
+                if ch == '|' {
+                    *close = Expect::Link2;
+                    compiler.index += 1;
+                } else if ch == ']' && compiler.peak("]]") {
+                    compiler.index += 2;
+                    compiler.lastrollbackindex.pop();
+                    return false;
+                } else {
+                    link.to.push(ch);
+                }
+                // println!("{}", ch); 이럼 왜 소유권 안넘어감??? A:Copy trait을 만족시켜서
+            } else {
+                panic!()
+            }
+        } else {
+            let mut thisparsing:Option<RenderObject> = None;
+            if ch == '[' && compiler.peak("[[") {
+                compiler.index += 2;
+                compiler.lastrollbackindex.push(compiler.index);
+                thisparsing = Some(parse_first(compiler, Expect::Link));
+            } else if ch == ']' && compiler.peak("]]") { //그냥 메크로는 간단한 파싱문구라서 메게변수 없는 건 여기서 처리하지 않는 것이 맞을듯...
+                if *close == Expect::Link2 {
+
+                } else if compiler.expected.contains(&Expect::Link2) { //Link1은 여기에 오면 안됨
+                    
+                } else {
+                    namumarkresult.push(Objects::Char(']'));
+                    namumarkresult.push(Objects::Char(']'));
+                }
+                compiler.index += 2;
+            } else {
+                namumarkresult.push(Objects::Char(ch));
+            }
+
+            return if let Some(rendobj) = thisparsing {
+                match rendobj {
+                    RenderObject::Nop(items) => {
+                        namumarkresult.extend(items);
+                        *result = RenderObject::Nop(namumarkresult.to_vec());
+                        false
+                    },
+                    RenderObject::NopForLink => {
+                        if *close == Expect::Link2 && compiler.lastrollbackindex.len() == 1 {
+                            compiler.index = *compiler.lastrollbackindex.last().unwrap();
+                            return true
+                        }
+                        *result = RenderObject::NopForLink;
+                        return false;
+                    },
+                    RenderObject::EarlyParse(tuple) => {todo!()} //[[ {{{#!wiki 안녕]] }}} 대충 이런거 처리용
+                    RenderObject::NopNopNop => panic!("이게 뭐하는 베리언트였더라"),
+                    obj => {namumarkresult.push(Objects::RenderObject(obj)); true}
+                }
+            } else {
+                false
+            }
+        }
+    }
+    return true;
 }
--- src/structs.rs
+++ src/structs.rs
@@ -1,39 +1,42 @@
+use std::clone;
+
 use crate::parser::parse_first;
 
 #[warn(irrefutable_let_patterns)] //ㅖㅖ..
 #[derive(Debug)]
 pub struct Compiler {
-    index: usize,
+    pub index: usize,
     pub array: Vec<Objects>,
-    expected: Vec<Expect>,
-    pub lastrollbackindex:Vec<usize>
+    pub expected: Vec<Expect>,
+    pub lastrollbackindex: Vec<usize>,
 }
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Objects {
     Char(char),
     RenderObject(RenderObject),
 }
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum RenderObject {
     Link(Link),
     Nop(Vec<Objects>),
     NopForLink,
-    NopNopNop
+    NopNopNop,
+    EarlyParse((Expect, Vec<Objects>)) //우선순위 처리용
 }
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum LinkType {
     File,
     Hyper,
     Cat,
 }
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Link {
     pub to: String,
     pub show: Option<Vec<RenderObject>>,
     pub link_type: LinkType,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Expect {
     None,
     Link,
@@ -45,7 +48,7 @@ impl Compiler {
             index: 0,
             array: Vec::new(),
             expected: Vec::new(),
-            lastrollbackindex:Vec::new()
+            lastrollbackindex: Vec::new(),
         };
         for char in string.chars() {
             compiler.array.push(Objects::Char(char));
@@ -58,7 +61,21 @@ impl Compiler {
     fn get(&mut self, idx: usize) -> Option<&Objects> {
         self.array.get(idx)
     }
-    fn current(&mut self) -> Option<&Objects> {
+    pub fn current(&mut self) -> Option<&Objects> {
         self.array.get(self.index)
+    }
+    pub fn peak(&mut self, str: &str) -> bool {
+        let mut idx = 0;
+        for ch in str.chars() {
+            if let Some(Objects::Char(cha)) = self.get(self.index + idx) {
+                if ch != *cha {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+            idx += 1;
+        }
+        return true;
     }
 }
